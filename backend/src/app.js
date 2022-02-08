a1ae86b99409f9c70ed6be58ef6efceb669c449b
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-08 16:01:32 -0700

    created basic hello world server

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+module.exports = app;
